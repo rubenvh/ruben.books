1d8aaf6626130776bffad0263fd8f7d8599bcd79
rubenvh <rvhaverm@gmail.com>
2013-12-28 18:43:22 +0100

    allow creation of authors on the fly when creating a book

--- Ruben.Books.Web/Controllers/AuthorController.cs
+++ Ruben.Books.Web/Controllers/AuthorController.cs
@@ -4,16 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Ruben.Books.Repository;
+using Ruben.Books.Domain;
+using Ruben.Books.DataLayer;
 
 namespace Ruben.Books.Web.Controllers
 {
     public class AuthorController : Controller
     {
         private IAuthorRepository _authorRepository;
+        private IUnitOfWork<BooksContext> _unitOfWork;
 
-        public AuthorController(IAuthorRepository authorRepository)
+        public AuthorController(IAuthorRepository authorRepository, IUnitOfWork<BooksContext> unitOfWork)
         {
             _authorRepository = authorRepository;
+            _unitOfWork = unitOfWork;
         }
 
         //
@@ -24,6 +28,30 @@ namespace Ruben.Books.Web.Controllers
             var found = _authorRepository.FindAuthorByName(term);
             return Json(found.Select(_ => new {Id = _.Id, Name = _.Name}).ToList(),
                 JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult Create(string name)
+        {
+            var found = _authorRepository.FindAuthorByName(name);
+
+            int authorId = 0;
+            if (found.Count() == 0)
+            {
+                var author = new Author()
+                {
+                    State = State.Added,
+                    Name = name
+                };
+
+                _authorRepository.InsertOrUpdate(author);
+                _unitOfWork.Save();
+                authorId = author.Id;
+            }
+            else
+            {
+                authorId = found.First().Id;
+            }
+            return Json(new { Id = authorId, Name = name }, JsonRequestBehavior.AllowGet);
         }
 
     }
